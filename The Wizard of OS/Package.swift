99f3651f7c73b0e46c54a195f64b0030ef3e2cb0
DominiqueKarreman <s1169919@student.windesheim.nl>
2025-03-26 23:32:14 +0100

    Added package.swift

---
+++ The Wizard of OS/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "The Wizard Of OS", // Replace with your project name
+    platforms: [
+        .macOS(.v12), // You can change this to the appropriate platform(s) for your project
+        .iOS(.v14)    // Add more platforms as needed
+    ],
+    products: [
+        .library(
+            name: "The Wizard Of OS", // Replace with your project name
+            targets: ["The Wizard Of OS"]
+        ),
+    ],
+    dependencies: [
+    
+    ],
+    targets: [
+        .target(
+            name: "The Wizard Of OS", // Replace with your target name
+            dependencies: [] // Add any dependencies here if you have any
+        ),
+        .testTarget(
+            name: "The Wizard of OSTests", // Replace with your test target name
+            dependencies: ["The Wizard Of OS"] // Link the main target
+        ),
+        .testTarget(
+            name: "The Wizard of OSUITests", // UI Test target
+            dependencies: ["The Wizard Of OS"], // Link the main target (and potentially other dependencies)
+            
+        ),
+    ]
+)
