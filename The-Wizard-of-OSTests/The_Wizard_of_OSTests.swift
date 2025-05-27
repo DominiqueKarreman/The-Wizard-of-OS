f7e598beff78674a04e1accc184dcbdca9408287
DominiqueKarreman <s1169919@student.windesheim.nl>
2025-05-27 16:03:11 +0200

    Changes a whole lot in the app

---
+++ The-Wizard-of-OSTests/The_Wizard_of_OSTests.swift
@@ -0,0 +1,16 @@
+//
+//  The_Wizard_of_OSTests.swift
+//  The-Wizard-of-OSTests
+//
+//  Created by Dominique Karreman on 5/27/25.
+//
+
+import Testing
+
+struct The_Wizard_of_OSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ TheWizardOfOSTests/TheWizardOfOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  TheWizardOfOSTests.swift
+//  TheWizardOfOSTests
+//
+//  Created by Dominique Karreman on 5/27/25.
+//
+
+import Testing
+
+struct TheWizardOfOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
